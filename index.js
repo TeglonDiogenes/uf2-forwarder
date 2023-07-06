7c569205f29d724d341410f79048ebf04a09e50d
Your Name <you@example.com>
2023-07-06 18:59:17 +0000

    --wip-- [skip ci]

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-require('./get-config');
-const mqtt = require('mqtt');
-const fs = require('fs');
-const crypto = require('crypto');
-const debug = require('debug')('mqtt-to-binary');
+require('./get-config')
+const mqtt = require('mqtt')
+const fs = require('fs')
+const crypto = require('crypto')
+const debug = require('debug')('mqtt-to-binary')
 
-
-const {MQTT_BROKER_URL, MQTT_TOPIC, UPLOAD_DIRECTORY} = process.env;
-debug(MQTT_BROKER_URL, MQTT_TOPIC,UPLOAD_DIRECTORY,'zzz')
-const client = mqtt.connect(process.env.MQTT_BROKER_URL);
-
+const { MQTT_BROKER_URL, MQTT_BROADCAST_TOPIC, MQTT_TOPIC, UPLOAD_DIRECTORY } = process.env
+const client = mqtt.connect(MQTT_BROKER_URL)
 
 client.on('connect', () => {
-  client.subscribe(process.env.MQTT_TOPIC);
-});
+  client.subscribe(MQTT_TOPIC)
+  client.subscribe(MQTT_BROADCAST_TOPIC)
+})
 
 client.on('message', (topic, message) => {
-  const hash = crypto.createHash('md5').update(message).digest('hex');
-  const ts = Date.now();
-  const pathToFile = `${UPLOAD_DIRECTORY}/${ts}-${hash}.uf2`;
+  const hash = crypto.createHash('md5').update(message).digest('hex')
+  const ts = Date.now()
+  const pathToFile = `${UPLOAD_DIRECTORY}/${ts}-${hash}.uf2`
   fs.writeFile(pathToFile, message, (err) => {
-    if (err) throw err;
-    debug(`File ${pathToFile} created successfully`);
-  });
-});
+    if (err) throw err
+    debug(`File ${pathToFile} created successfully`)
+  })
+})
